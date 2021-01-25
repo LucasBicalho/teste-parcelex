d442421ae42c18fd2e21f23755a9cd73383c7eac
Lucas Bicalho <lucassbcosta@hotmail.com>
2021-01-25 02:14:08 -0300

    refactor: moving everything to src folder

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 
-import { Header, Footer } from "./components";
+import { Header, Footer } from "../components";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="app">
-      <Header cartNumber={12} />
-      <Footer />
-    </div>
+      <div className="app">
+        <Header cartNumber={0} />
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
