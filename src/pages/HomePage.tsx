f6bcf2e09b8033c7e891a28d4f4d369edd297a68
Lucas Bicalho <lucassbcosta@hotmail.com>
2021-01-29 20:12:24 -0300

    feat: update homepage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,11 +2,16 @@ import React from "react";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Main from "../components/Main";
+import NavBar from "../components/NavBar";
+import { MenuLinkType } from "../types/MenuItems";
 
 const HomePage: React.FC = () => {
   return (
     <div className="app">
       <Header cartNumber={0} />
+      <NavBar activeLinkId={MenuLinkType.home} />
+      <Main />
       <Footer />
     </div>
   );
