2b31a99448c94d8b69b65f24eb88ed0bd1b759a7
Lucas Bicalho <lucassbcosta@hotmail.com>
2021-01-29 19:51:03 -0300

    feat: add template html for ssr

---
+++ src/Template.ts
@@ -0,0 +1,31 @@
+class Template {
+  getHTML(body: string, styles: string, scripts = "") {
+    const html = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="Web site created using create-react-app"
+          />
+          <title>Parcelex</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com">
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+          <style>${styles}</style>
+        </head>
+        <body>
+          <div id="root">
+            ${body}
+          </div>
+          ${scripts}
+          <script src="bundle_client.js"></script>
+        </body>
+      </html>
+    `;
+    return html;
+  }
+}
+
+export default new Template();
